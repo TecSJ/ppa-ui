dd82386799e8777f5bc5864cd588b32202c2d689
Zenitramhgx <zenitramhgx15@hotmail.com>
2025-03-05 13:11:54 -0600

    Cambios mínimos.

--- app/shared/common/Papers/AnimatedPaper.tsx
+++ app/shared/common/Papers/AnimatedPaper.tsx
@@ -2,39 +2,26 @@
 
 import { Paper } from '@mui/material';
 import { motion } from 'framer-motion';
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode } from 'react';
 
 interface AnimatedPaperProps {
   children: ReactNode;
 }
 
 export default function AnimatedPaper({ children }: AnimatedPaperProps) {
-  const [maxHeight, setMaxHeight] = useState('90vh');
-
-  useEffect(() => {
-    const updateMaxHeight = () => {
-      setMaxHeight(`${window.innerHeight * 0.9}px`);
-    };
-
-    updateMaxHeight();
-    window.addEventListener('resize', updateMaxHeight);
-
-    return () => window.removeEventListener('resize', updateMaxHeight);
-  }, []);
-
   return (
     <motion.div
       initial={{ scale: 0.5, opacity: 0 }}
       animate={{ scale: 1.05, opacity: 1 }}
       transition={{ duration: 0.5, ease: 'easeOut' }}
-      whileHover={{ scale: 1.02 }}
-      className='flex justify-center items-center w-full h-full'
+      className='flex justify-center items-center w-full'
+      style={{ height: '100%', marginRight: '-35px' }}
     >
       <Paper
         elevation={4}
         sx={{
-          width: '90%',
-          maxHeight,
+          width: '95%',
+          height: '95%',
           minHeight: '200px',
           padding: 4,
           position: 'relative',
